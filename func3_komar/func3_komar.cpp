213b72d719107412e844a206e59bb424b0bf2e96
V1PeRI4 <V1PeRI4@DESKTOP-C59PJAE>
2021-11-22 21:46:57 +0300

    Добавьте файлы проекта.

---
+++ func3_komar/func3_komar.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int Text1();
+int Text2();
+
+int main()
+{
+    Text1();
+    Text1();
+    Text2();
+    Text2();
+    return 0;
+}
+
+
+int Text1 () {
+    cout << "Three blind mice" << endl;
+    return 0;
+}
+
+int Text2() {
+    cout << "See how they run" << endl;
+    return 0;
+}
